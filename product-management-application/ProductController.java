74b8402a0777186be2211c5c53de1ff4c1b4285c
vinitkit <160644445+vinitkit@users.noreply.github.com>
2024-02-23 16:54:29 +0530

    Add files via upload

---
+++ product-management-application/ProductController.java
@@ -0,0 +1,33 @@
+package com.kit.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.kit.entity.Product;
+import com.kit.service.ProductService;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+public class ProductController {
+
+    @Autowired
+    ProductService service;
+
+    // hey, this is for products request
+    @GetMapping("/products")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
+    }
+
+    @GetMapping("/product/{name}")
+    public Product getProduct(@PathVariable String name){
+        return service.getProduct(name);
+    }
+
+    @PostMapping("/product")
+    public void addProduct(@RequestBody Product p){
+        service.addProduct(p);
+    }
+}
