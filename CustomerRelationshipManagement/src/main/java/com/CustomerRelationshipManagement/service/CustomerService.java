66373b47b7efa42ecadd9e82175cb1aebfe306ab
vinitkit <160644445+vinitkit@users.noreply.github.com>
2024-02-28 16:45:15 +0530

    Update CustomerService.java

--- CustomerRelationshipManagement/src/main/java/com/CustomerRelationshipManagement/service/CustomerService.java
+++ CustomerRelationshipManagement/src/main/java/com/CustomerRelationshipManagement/service/CustomerService.java
@@ -5,5 +5,8 @@ import com.CustomerRelationshipManagement.entity.Customer;
 public interface CustomerService {
 	
 	String insertCustomer(Customer customer);
+	public List<Customer> getAllCustomers();
+	public Customer getCustomerById(int id);
+	public void  deleteCustomerById(int id);
 
 }
