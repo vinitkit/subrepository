a852b993b9c519401728cc927f42bfb3f5fdd723
vinitkit <160644445+vinitkit@users.noreply.github.com>
2024-02-28 16:43:34 +0530

    Update CustomerController.java

--- CustomerRelationshipManagement/src/main/java/com/CustomerRelationshipManagement/controller/CustomerController.java
+++ CustomerRelationshipManagement/src/main/java/com/CustomerRelationshipManagement/controller/CustomerController.java
@@ -27,4 +27,40 @@ public class CustomerController {
 		return msg;
 		
 	}
+	@GetMapping("/all")
+	public List<Customer>getAllCustomers(){
+		return customerService.getAllCustomers();
+	}
+	
+	@GetMapping("/get/{id}")
+	public Customer getCustomer (@PathVariable int id) {
+		Customer customer =customerService.getCustomerById(id);
+		
+		return customer;
+	}
+		
+		
+
+	    @PutMapping("/update/{id}")
+	    public ResponseEntity<?> updateEntity(@PathVariable int id, @RequestBody Customer customer) {
+	        Optional<Customer> optionalEntity = customerDao.findById(id);
+	        if (!optionalEntity.isPresent()) {
+	            return ResponseEntity.notFound().build();
+	        }
+
+	        Customer existingEntity = optionalEntity.get();
+	        existingEntity.setAge(customer.getAge());
+	        existingEntity.setFirstName(customer.getFirstName());
+	        existingEntity.setLastName(customer.getLastName());
+	        existingEntity.setEmail(customer.getEmail());
+	        existingEntity.setMobileNumber(customer.getMobileNumber());
+	        customerDao.save(existingEntity); // Save the updated entity
+
+	        return ResponseEntity.ok(existingEntity);
+	    }
+	    
+	    @DeleteMapping("/delete/{id}")
+	    public void deleteCustomerById(@PathVariable int id) {
+	    	customerService.deleteCustomerById(id);
+	    }
 }
